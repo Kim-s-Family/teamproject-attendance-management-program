b043ff43c69d979e566a855ff546724173fc6245
rlawlsdyd5932 <jyjy0509@naver.com>
2024-02-25 17:41:04 +0900

    second draft commit

--- src/main/java/com/ohgiraffers/model/dao/EmployeeDAO.java
+++ src/main/java/com/ohgiraffers/model/dao/EmployeeDAO.java
@@ -2,6 +2,7 @@ package com.ohgiraffers.model.dao;
 
 
 import com.ohgiraffers.model.dto.EmployeeDTO;
+import com.ohgiraffers.model.dto.EmployeeDTO2;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -33,7 +34,7 @@ public class EmployeeDAO {
         }
     }
 
-    public int insertNewMenu(Connection con, EmployeeDTO newEmployee) {
+    public int insertNewEmployee(Connection con, EmployeeDTO newEmployee) {
 
         PreparedStatement pstmt = null;
         System.out.println("newEmployee = " + newEmployee);
@@ -48,11 +49,14 @@ public class EmployeeDAO {
             pstmt.setString(2, newEmployee.getEmpPwd());
             pstmt.setString(3, newEmployee.getEmpName());
             pstmt.setString(4, newEmployee.getPhone());
-            pstmt.setString(6, newEmployee.getEmail());
-            pstmt.setString(7, newEmployee.getDepartmentCode());
-            pstmt.setString(8, newEmployee.getPositionCode());
-            pstmt.setString(9, newEmployee.getEmpIdentification());
-            pstmt.setString(10, newEmployee.getPaymentCode());
+            pstmt.setString(5, newEmployee.getEmail());
+            pstmt.setString(6, newEmployee.getDepartmentCode());
+            pstmt.setString(7, newEmployee.getPositionCode());
+            pstmt.setString(8, newEmployee.getEmpIdentification());
+            pstmt.setString(9, newEmployee.getPaymentCode());
+
+
+
 
             result = pstmt.executeUpdate();
 
@@ -63,16 +67,71 @@ public class EmployeeDAO {
         }
 
         return result;
-
-
     }
 
-
-
-
-
-
-
+//    public int updateEmpInfo(Connection con, EmployeeDTO changeInfo) {
+//
+//        PreparedStatement pstmt = null;
+//        System.out.println("changeInfo = " + changeInfo);
+//
+//        int result = 0;
+//
+//        String query = prop.getProperty("updateEmpInfo");
+//
+//        try {
+//            pstmt = con.prepareStatement(query);
+//            pstmt.setString(1, changeInfo.getEmpId());
+//            pstmt.setString(2, changeInfo.getEmpPwd());
+//            pstmt.setString(3, changeInfo.getEmpName());
+//            pstmt.setString(4, changeInfo.getPhone());
+//            pstmt.setString(5, changeInfo.getEmail());
+//            pstmt.setString(6, changeInfo.getDepartmentCode());
+//            pstmt.setString(7, changeInfo.getPositionCode());
+//            pstmt.setString(8, changeInfo.getEmpIdentification());
+//            pstmt.setString(9, changeInfo.getPaymentCode());
+//
+//            result = pstmt.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        } finally {
+//            close(pstmt);
+//        }
+//
+//        return result;
+//
+//    }
+//
+//    public int updateAttendanceRecord(Connection con, EmployeeDTO2 changeRecord) {
+//
+//        PreparedStatement pstmt = null;
+//        System.out.println("changeRecord = " + changeRecord);
+//
+//        int result = 0;
+//
+//        String query = prop.getProperty("updateAttendanceRecord");
+//
+//        try {
+//            pstmt = con.prepareStatement(query);
+//            pstmt.setString(1, changeRecord.getAttendanceCode());
+//            pstmt.setString(2, changeRecord.getEmpid());
+//            pstmt.setString(3, String.valueOf(changeRecord.getArrivalAtWork()));
+//            pstmt.setString(4, String.valueOf(changeRecord.getLeaveWork()));
+//            pstmt.setString(5, String.valueOf(changeRecord.getLateWork()));
+//            pstmt.setString(6, changeRecord.getVacation());
+//            pstmt.setString(7, String.valueOf(changeRecord.getDayOff()));
+//
+//            result = pstmt.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        } finally {
+//            close(pstmt);
+//        }
+//
+//        return result;
+//
+//    }
 }
 
 
--- src/main/java/com/ohgiraffers/test/TestApplication.java
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -1,5 +1,9 @@
 package com.ohgiraffers.test;
 
+import com.ohgiraffers.model.dao.EmployeeDAO;
+import com.ohgiraffers.model.dto.EmployeeDTO;
+import com.ohgiraffers.model.dto.EmployeeDTO2;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,24 +19,112 @@ public class TestApplication {
     public static void main(String[] args) {
 
         Connection con = getConnection();
+        PreparedStatement pstmt = null;
 
-        Properties prop = new Properties();
+        /*정보입력 insertNewEmployee*/
+        EmployeeDAO registerDAO = new EmployeeDAO();
+        EmployeeDTO newEmployee = new EmployeeDTO();
 
-        PreparedStatement pstmt = null;  //메뉴를 추가
+        int result = registerDAO.insertNewEmployee(con, newEmployee);
 
-        int result = 0;
 
         Scanner sc = new Scanner(System.in);
         System.out.print("등록할 직원ID를 입력하세요: ");
-        String empid = sc.nextLine();
+        String empId = sc.nextLine();
         System.out.print("등록할 직원PWD를 입력하세요: ");
-        String emppwd = sc.nextLine();
-        System.out.print("카테고리를 선택해주세요(식사,음료,디저트,한식,퓨전): ");
-        sc.nextLine();
-        String categoryName = sc.nextLine();
-        System.out.print("바로 판매 메뉴에 적용하시겠습니까?(에/아니오): ");
-        String answer = sc.nextLine();
+        String empPWD = sc.nextLine();
+        System.out.print("등록할 직원 이름을 입력하세요: ");
+        String empName = sc.nextLine();
+        System.out.print("등록할 연락처를 입력하세요: ");
+        String phone = sc.nextLine();
+        System.out.print("등록할 이메일을 입력하세요: ");
+        String email = sc.nextLine();
+        System.out.print("등록할 부서코드(DP1 ~ DP5) 중 하나를 입력하세요: ");
+        String departmentCode = sc.nextLine();
+        System.out.print("등록할 직책코드(PS1 ~ PS5) 중 하나를 입력하세요: ");
+        String positionCode = sc.nextLine();
+        System.out.print("사원분류(admin,mgr,emp) 중 하나를 입력해주세요: ");
+        String empIdentification = sc.nextLine();
+        System.out.print("등록할 급여코드(PM1 ~ PM5) 중 하나를 입력하세요: ");
+        String paymentCode = sc.nextLine();
+
+        System.out.println("Employee ID: "+ empId);
+        System.out.println("emp PWD: "+ empPWD);
+        System.out.println("phone: "+empName);
+        System.out.println("email: "+phone);
+        System.out.println("departmentCode: "+email);
+        System.out.println("positionCode: "+positionCode);
+        System.out.println("empIdentification: "+empIdentification);
+        System.out.println("paymentCode: "+ paymentCode);
+        if (result > 0) {
+            System.out.println("신규 직원 등록 성공!");
+        } else {
+            System.out.println("신규 직원 등록 실패!");
+        }
+
+//        /*정보수정 changeEmpInfo*/
+//
+//        EmployeeDAO changeDAO = new EmployeeDAO();
+//        EmployeeDTO changeInfo = new EmployeeDTO();
+//
+//        result = changeDAO.updateEmpInfo(con, changeInfo);
+//
+//
+//        System.out.print("변경할 직원ID를 입력하세요: ");
+//        empId = sc.nextLine();
+//        System.out.print("변경할 직원PWD를 입력하세요: ");
+//        empPWD = sc.nextLine();
+//        System.out.print("변경할 직원 이름을 입력하세요: ");
+//        empName = sc.nextLine();
+//        System.out.print("변경할 연락처를 입력하세요: ");
+//        phone = sc.nextLine();
+//        System.out.print("변경할 이메일을 입력하세요: ");
+//        email = sc.nextLine();
+//        System.out.print("변경할 부서코드(DP1 ~ DP5) 중 하나를 입력하세요: ");
+//        departmentCode = sc.nextLine();
+//        System.out.print("변경할 직책코드(PS1 ~ PS5) 중 하나를 입력하세요: ");
+//        positionCode = sc.nextLine();
+//        System.out.print("변경할 사원분류(admin,mgr,emp) 중 하나를 입력해주세요: ");
+//        empIdentification = sc.nextLine();
+//        System.out.print("변경할 급여코드(PM1 ~ PM5) 중 하나를 입력하세요: ");
+//        paymentCode = sc.nextLine();
+//
+//        if (result > 0) {
+//            System.out.println("변경 성공!");
+//        } else {
+//            System.out.println("변경 실패!");
+//        }
+//
+//        /*정보수정 changeAttendanceRecord*/
+//
+//        EmployeeDAO updateDAO = new EmployeeDAO();
+//        EmployeeDTO2 changeRecord = new EmployeeDTO2();
+//
+//        result = updateDAO.updateAttendanceRecord(con, changeRecord);
+//
+//        System.out.print("변경할 근태코드를 입력하세요: ");
+//        String attendanceCode = sc.nextLine();
+//        System.out.print("변경할 직원ID를 입력하세요: ");
+//        empId = sc.nextLine();
+//        System.out.print("출근정보를 변경하시겠습니까?(Y/N) ");
+//        String arrivalAtWork = sc.nextLine();
+//        System.out.print("퇴근정보를 변경하시겠습니까?(Y/N) ");
+//        String leaveWork = sc.nextLine();
+//        System.out.print("지각정보를 변경하시겠습니까?(Y/N) ");
+//        String lateWork= sc.nextLine();
+//        System.out.print("휴가정보를 변경하시겠습니까?(Y/N) ");
+//        String vacation = sc.nextLine();
+//        System.out.print("결근정보를 변경하시겠습니까?(Y/N) ");
+//        String dayOff = sc.nextLine();
+//
+//        if (result > 0) {
+//            System.out.println("변경 성공!");
+//        } else {
+//            System.out.println("변경 실패!");
+//        }
+
     }
+
 }
 
 
