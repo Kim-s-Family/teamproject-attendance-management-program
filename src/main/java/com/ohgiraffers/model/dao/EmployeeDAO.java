097afa796ff39c987ae5ce54e0e4c2e5df972321
manho <krossjsnet88@gmail.com>
2024-02-25 23:59:50 +0900

    control commit

--- src/main/java/com/ohgiraffers/model/dao/EmployeeDAO.java
+++ src/main/java/com/ohgiraffers/model/dao/EmployeeDAO.java
@@ -1,4 +1,35 @@
 package com.ohgiraffers.model.dao;
 
+import com.ohgiraffers.model.dto.EmployeeDTO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
+
+import static com.ohgiraffers.common.JDBCTemplate.close;
+import static com.ohgiraffers.common.JDBCTemplate.getConnection;
+
+
 public class EmployeeDAO {
+
+    private Properties prop = new Properties();
+
+    /* xml 파일을 불러오는 기본생성자 생성 */
+    public EmployeeDAO() {
+        try {
+            prop.loadFromXML(new FileInputStream("src/main/java/com/ohgiraffers/mapper/attendance-management-query.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
+
+
--- src/main/java/com/ohgiraffers/test/TestApplication.java
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -1,9 +1,25 @@
 package com.ohgiraffers.test;
 
+import com.ohgiraffers.control.MainMenu;
+import com.ohgiraffers.model.dao.EmployeeDAO;
+
+import static com.ohgiraffers.view.AsciiArtPrinter.startLogo;
+
 public class TestApplication {
 
     public static void main(String[] args) {
 
+
+        startLogo();
+
+        try {
+            Thread.sleep(3500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        MainMenu loginEmp = new MainMenu();
+        loginEmp.login();
 
 
     }
