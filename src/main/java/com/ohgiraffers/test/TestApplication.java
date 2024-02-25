b56989b525922abaca69ade7f86a9f3afb5a31c6
LEEMINKUK <cjcjcj963@gmail.com>
2024-02-25 18:30:32 +0900

    change

--- src/main/java/com/ohgiraffers/test/TestApplication.java
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -1,8 +1,16 @@
 package com.ohgiraffers.test;
 
+import com.ohgiraffers.model.dao.EmployeeDAO;
+import com.ohgiraffers.model.dto.EmployeeDTO;
+
 public class TestApplication {
 
     public static void main(String[] args) {
+
+        EmployeeDAO employeeDAO = new EmployeeDAO();
+
+
+        employeeDAO.changeEmpInfo();
 
 
 
